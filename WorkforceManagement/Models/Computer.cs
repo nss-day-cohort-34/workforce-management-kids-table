cbcd066ea8b75c9ce199a77f35ea22e23f934f00
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-11-15 11:04:40 -0600

    added models for computer, department, employee, and training program

---
+++ WorkforceManagement/Models/Computer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkforceManagement.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Manufacturer { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public DateTime? DecommissionDate { get; set; }
+
+    }
+}
---
+++ WorkforceManagement/Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkforceManagement.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Budget { get; set; }
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+
+    }
+}
