f1970593124dd0ca8925c966e7533712ba0e7704
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-11-18 11:07:57 -0600

    completed method to assign employee to training program. Added view and view model

---
+++ WorkforceManagement/Models/ViewModels/EmployeeAssignTrainingProgramViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkforceManagement.Models.ViewModels
+{
+    public class EmployeeAssignTrainingProgramViewModel
+    {
+        public Employee Employee { get; set; }
+        public List<TrainingProgram> AllTrainingPrograms { get; set; } = new List<TrainingProgram>();
+        public List<int> SelectedTrainingProgramIds { get; set; } = new List<int>();
+
+        public List<SelectListItem> TrainingProgramOptions
+        {
+            get
+            {
+                if (AllTrainingPrograms == null) return null;
+
+                return AllTrainingPrograms
+                    .Select(t => new SelectListItem(t.Name, t.Id.ToString()))
+                    .ToList();
+            }
+        }
+    }
+}
