bd42acd4ea4cce2e59829f168fb8849bfc829907
mjimeneztn <mjimeneztn@gmail.com>
2019-11-18 11:55:28 -0600

    filter is complete. unassign and assign works. functionality works, but error message is still in progress

--- WorkforceManagement/Controllers/ComputerController.cs
+++ WorkforceManagement/Controllers/ComputerController.cs
@@ -30,49 +30,62 @@ namespace WorkforceManagement.Controllers
             }
         }
         // GET: Computer
-        public ActionResult Index()
+        public ActionResult Index(string searchString)
         {
             using (SqlConnection conn = Connection)
             {
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = @"SELECT c.Id as 'ComputerId', c.Make, c.Manufacturer, 
+                    if (!String.IsNullOrEmpty(searchString))
+                    {
+                        cmd.CommandText = @"SELECT c.Id as 'ComputerId', c.Make, c.Manufacturer, 
+                         e.id as 'EmployeeId', e.FirstName, e.LastName, ce.AssignDate, ce.UnassignDate
+                        FROM Computer c LEFT JOIN ComputerEmployee ce ON CE.ComputerId = c.Id
+                         LEFT JOIN Employee e ON ce.employeeId = e.Id WHERE Make LIKE @searchString OR Manufacturer LIKE @searchString";
+                        cmd.Parameters.Add(new SqlParameter("@searchString", searchString));
+                    }
+                    else
+                    {
+                        cmd.CommandText = @"SELECT c.Id as 'ComputerId', c.Make, c.Manufacturer, 
                          e.id as 'EmployeeId', e.FirstName, e.LastName, ce.AssignDate, ce.UnassignDate
                         FROM Computer c LEFT JOIN ComputerEmployee ce ON CE.ComputerId = c.Id
                          LEFT JOIN Employee e ON ce.employeeId=e.Id";
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    List<Computer> computers = new List<Computer>();
-                    Computer computer = null;
-                    Employee employee = null;
-
-                    while (reader.Read())
-                    {
-                        computer = new Computer
-                        {
-                            Id = reader.GetInt32(reader.GetOrdinal("ComputerId")),
-                            Make = reader.GetString(reader.GetOrdinal("Make")),
-                            Manufacturer = reader.GetString(reader.GetOrdinal("Manufacturer"))
-                        };
-
-
-                        computers.Add(computer);
-                        if (reader.IsDBNull(reader.GetOrdinal("UnassignDate")) && !reader.IsDBNull(reader.GetOrdinal("assignDate")))
-                        {
-                            employee = new Employee
-                            {
-                                Id = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
-                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
-                                LastName = reader.GetString(reader.GetOrdinal("LastName"))
-                            };
-                            computer.employee = employee;
-                        }
                     }
-                    reader.Close();
-                    return View(computers);
+                        SqlDataReader reader = cmd.ExecuteReader();
+                        List<Computer> computers = new List<Computer>();
+                        Computer computer = null;
+                        Employee employee = null;
+                    
+
+                        while (reader.Read())
+                        {
+                            computer = new Computer
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("ComputerId")),
+                                Make = reader.GetString(reader.GetOrdinal("Make")),
+                                Manufacturer = reader.GetString(reader.GetOrdinal("Manufacturer"))
+                            };
+
+
+                            computers.Add(computer);
+                            if (reader.IsDBNull(reader.GetOrdinal("UnassignDate")) && !reader.IsDBNull(reader.GetOrdinal("assignDate")))
+                            {
+                                employee = new Employee
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
+                                    FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                    LastName = reader.GetString(reader.GetOrdinal("LastName"))
+                                };
+                                computer.employee = employee;
+                            }
+                        }
+                        reader.Close();
+                        return View(computers);
+                    }
                 }
             }
-        }
+        
 
 
         // GET: Computer/Details/5
@@ -117,9 +130,9 @@ namespace WorkforceManagement.Controllers
                     using (SqlCommand cmd = conn.CreateCommand())
                     {
                         cmd.CommandText = @"INSERT INTO Computer 
-                           (Make, Manufacturer, PurchaseDate, DecommissionDate)
+                           (Make, Manufacturer, PurchaseDate)
                                               OUTPUT INSERTED.Id
-                                                VALUES (@make, @manufacturer, @purchaseDate, null)";
+                                                VALUES (@make, @manufacturer, @purchaseDate)";
                         cmd.Parameters.Add(new SqlParameter("@make", viewModel.computer.Make));
                         cmd.Parameters.Add(new SqlParameter("@manufacturer", viewModel.computer.Manufacturer));
                         cmd.Parameters.Add(new SqlParameter("@purchaseDate", viewModel.computer.PurchaseDate));
